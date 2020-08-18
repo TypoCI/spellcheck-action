d45283adfb65ad500580a2cc704e02813ea47553
Mike Rogers <me@mikerogers.io>
2020-08-18 16:51:59 +0100

    bug: Only run on events that have a git push involved (#8)
    * Adding failing spec for events we shouldn't act on
    
    * Getting tests to pass

--- app/models/github/event/pull_request.rb
+++ app/models/github/event/pull_request.rb
@@ -1,4 +1,12 @@
 class Github::Event::PullRequest < Github::Event
+  def supported_action?
+    action.in?(%w[opened synchronize])
+  end
+
+  def action
+    data.dig(:action)
+  end
+
   def pull_request_number
     data.dig(:number)
   end
@@ -8,8 +16,8 @@ class Github::Event::PullRequest < Github::Event
   end
 
   def to_h
-    super.merge({
-                  pull_request_number: pull_request_number
-                })
+    super.merge(
+      pull_request_number: pull_request_number
+    )
   end
 end
--- app/models/github/event/push.rb
+++ app/models/github/event/push.rb
@@ -1,4 +1,12 @@
 class Github::Event::Push < Github::Event
+  def supported_action?
+    true
+  end
+
+  def action
+    'push'
+  end
+
   def base_sha
     data.dig(:before)
   end
