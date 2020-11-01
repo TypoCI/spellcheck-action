fcd227d4454eda879977e0cfc20c52699c7eb509
Mike Rogers <me@mikerogers.io>
2020-11-01 16:16:07 +0000

    Putting information about the repo to see what's in there

--- app/models/github/event.rb
+++ app/models/github/event.rb
@@ -6,7 +6,6 @@ class Github::Event < ApplicationRecord
 
   def initialize(github_event_path:)
     @github_event_path = github_event_path || ENV['GITHUB_EVENT_PATH']
-    TypoCi::Logger.info(data.inspect)
   end
 
   def head_sha
@@ -25,6 +24,8 @@ class Github::Event < ApplicationRecord
       head_branch: head_branch,
       head_sha: head_sha,
       actor: actor,
+      sender_type: sender_type,
+      repository_private: repository_private,
       pull_request_number: nil
     }
   end
@@ -49,6 +50,14 @@ class Github::Event < ApplicationRecord
 
   def head_branch
     ENV['GITHUB_HEAD_REF']
+  end
+
+  def sender_type
+    data.dig(:sender, :type)
+  end
+
+  def repository_private
+    data.dig(:repository, :private)
   end
 
   private
