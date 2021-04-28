00cbecf7a183983117fb061f011dc24778a45949
Mike Rogers <me@mikerogers.io>
2021-04-28 09:50:07 +0100

    Using CSpell dictionaries over relying on maintaining my own

--- app/services/dictionaries/combine_service.rb
+++ app/services/dictionaries/combine_service.rb
@@ -3,12 +3,14 @@
 class Dictionaries::CombineService
   def initialize
     @input_directory = Rails.root.join('db', 'dict', 'contextual')
+    @cspell_input_directory = Rails.root.join('node_modules', '@cspell')
     @output_directory = Rails.root.join('db', 'dict', 'combined_contextual')
     @words = []
   end
 
-  def call
+  def run!
     build_word_list
+    add_words_from_cspell
     write_word_list
   end
 
@@ -18,6 +20,17 @@ class Dictionaries::CombineService
     @words = @input_directory.glob('*.dic').collect do |dictionary_file|
       dictionary_file.readlines.drop(1).collect(&:strip)
     end.flatten.uniq.sort.select(&:present?)
+  end
+
+  def add_words_from_cspell
+    %w[typescript html-symbol-entities fonts software-terms scala rust ruby python powershell php npm node fullstack
+       elixir django dotnet companies bash aws].each do |language|
+      @words += @cspell_input_directory.join("dict-#{language}").glob('*.txt.gz').collect do |file|
+        Zlib::GzipReader.open(file).readlines.collect(&:strip)
+      end.flatten
+    end
+
+    @words.flatten.uniq.sort
   end
 
   def write_word_list
